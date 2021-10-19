5d365f5e33ddc6b5102840a93c167b02492d6ec3
GeordieR <40744776+Geordie-R@users.noreply.github.com>
2021-10-20 00:22:34 +0100

    Adding Questions to upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -23,7 +23,7 @@ fi
 
 #Parameters
 filename="/etc/systemd/system/cnode.service"
-
+testnet_version=1.5.0
 #Install JQ which makes it easy to interpret JSON
 apt-get update -y
 apt-get install -y jq
@@ -83,26 +83,72 @@ done
 
 
 
+function removequotes(){
+  #Remove the front and end double quote
+  version=${1#"\""}
+  version=${version%"\""}
+  echo "$version"
+}
+
+new_version_tag_final=""
+new_version_tag=$(curl -s https://api.github.com/repos/coti-io/$node_folder/releases/latest | jq ".tag_name")
+
+
+#Remove the front and end double quote
+new_version_tag=$(removequotes "$new_version_tag")
 
 
 
+echo "Welcome to the COTI Node Upgrader .  We will begin to ask you a series of questions.  Please have to hand:"
+echo "✅ Your Ubuntu Username (if you accepted defaults on the original install its likely to be: coti)"
+echo "✅ The version number you have been told to upgrade to"
+
+read -n 1 -r -s -p $'Press enter to begin...\n'
+
+read -p "What is your ubuntu username (hint its not root, likely to be coti if you accepted defaults on original install) ?: " username
+
+extra_vers_desc=""
+if [[ $action == "testnet" ]];
+then
+extra_vers_desc="If you leave this empty, it will use the latest version."
+elif [[ $action == "mainnet" ]];
+then
+extra_vers_desc="If you leave this empty, the script will terminate."
+
+fi
+
+read -p "What version node software would you like to upgrade to? If you are on mainnet, or if you are an exchange, this should have been communicated to you from COTI. $extra_vers_desc. If entering a version number, remember it takes this format: 1.4.1 ?: " new_version_tag_final
+
+
+# If we are on mainnet and a version isnt chosen, terminate the script
+if [[ $action == "mainnet" ]] && [[ $new_version_tag_final == "" ]];
+then
+echo "${RED}No version chosen.  Terminating script. ${COLOR_RESET}"
+exit 1
+elif [[ $action == "testnet" ]] && [[ $new_version_tag_final == "" ]];
+then
+echo "${YELLOW}No version chosen, that's ok, selecting latest version.${COLOR_RESET}"
+new_version_tag_final=$testnet_version
+fi
+
+
+# Finally if the user wrote 'latest' then it will pull the latest version!
+if [[ $new_version_tag_final == "latest" ]];
+then
+new_version_tag_final=$new_version_tag
+fi
+
+echo "Chosen version is $new_version_tag_final"
+
+if [[ $username == "" ]];
+then
+echo "${RED}Some details like the username were not provided.  Script is now exiting.  Please run again and provide answers to all of the questions${COLOR_RESET}"
+exit 1
+fi
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-cd /home/coti/coti-fullnode/
+cd /home/$username/coti-fullnode/
 
 #Lets check if we actually need to git pull
 changed=0
@@ -116,15 +162,8 @@ fi
 
 #Now we will go on to check the cnode.service file and update it if necessary
 
-#new_version_tar=$(curl -s https://api.github.com/repos/coti-io/coti-node/releases/latest | jq ".tarball_url")
-new_version_tag=$(curl -s https://api.github.com/repos/coti-io/coti-node/releases/latest | jq ".tag_name")
 
-#Remove the front and end double quote
-new_version_tag=${new_version_tag#"\""}
-new_version_tag=${new_version_tag%"\""}
-
-
-new_jar_name="fullnode-$new_version_tag.RELEASE.jar"
+new_jar_name="fullnode-$new_version_tag_final.RELEASE.jar"
 
 #Get full cnode.service inside a variable
 cnode_file=$(<$filename)
